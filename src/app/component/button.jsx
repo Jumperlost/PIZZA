311df2602cb411d0df2336c5f59005c5e4e0d102
Jumperlost <kosver00795@gmail.com>
2024-03-30 23:59:37 +0200

    pizza_counter

--- src/app/component/button.jsx
+++ src/app/component/button.jsx
@@ -1,8 +1,24 @@
+import React, { useState } from "react";
+import Counter from "./countPizzas";
+
 const Button = () => {
+  const [isVisible, setVisible] = useState(true);
+  const handClick = () => {
+    setVisible(false);
+  };
   return (
-    <button className="px-3 py-3 h-max rounded-lg bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-300 focus:ring focus:outline-none ease-in duration-300">
-      Add to cart
-    </button>
+    <>
+      {isVisible === true ? (
+        <button
+          onClick={handClick}
+          className="px-3 py-3 h-max rounded-lg bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-300 focus:ring focus:outline-none ease-in duration-300"
+        >
+          Add to cart
+        </button>
+      ) : (
+        <Counter></Counter>
+      )}
+    </>
   );
 };
 
--- src/app/component/countPizzas.jsx
+++ src/app/component/countPizzas.jsx
@@ -1,17 +1,40 @@
 "use client";
 import React, { useState } from "react";
+import Button from "./button";
+import { IoAddCircleOutline } from "react-icons/io5";
+import { CiCircleMinus } from "react-icons/ci";
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
+  const [isVisible, setVisible] = useState(false);
+  const handClick = () => {
+    setVisible(true);
+  };
+
+  const [count, setCount] = useState(1);
   const increment = () => setCount((prevCount) => prevCount + 1);
   const decrement = () => setCount((prevCount) => Math.max(prevCount - 1, 0));
-  const reset = () => setCount(0);
+  const reset = () => setCount((prevCount) => prevCount - prevCount);
   return (
     <div>
-      <button onClick={decrement}>-</button>
-      <span>{count}</span>
-      <button onClick={increment}>+</button>
-      <button onClick={reset}>Delete</button>
+      {isVisible || count === 0 ? (
+        <Button onClick={handClick}></Button>
+      ) : (
+        <div className="flex flex-row gap-2  justify-center items-center">
+          <button onClick={decrement}>
+            <CiCircleMinus className="hover:scale-125" />
+          </button>
+          <span className="select-none">{count}</span>
+          <button onClick={increment}>
+            <IoAddCircleOutline className="hover:scale-125" />
+          </button>
+          <button
+            className="px-3 py-3 h-max rounded-lg bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-300 focus:ring focus:outline-none ease-in duration-300"
+            onClick={reset}
+          >
+            Delete
+          </button>
+        </div>
+      )}
     </div>
   );
 };
